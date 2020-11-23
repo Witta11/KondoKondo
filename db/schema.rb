732df4414316c4ee833ef843421adb04df418dd8
Witta Glöckner <alina.glocke@gmail.com>
2020-11-23 18:08:31 +0100

    deleting favourites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_165615) do
+ActiveRecord::Schema.define(version: 2020_11_23_170716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 2020_11_23_165615) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["kondo_id"], name: "index_favorites_on_kondo_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
-  end
-
-  create_table "favourites", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "kondo_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["kondo_id"], name: "index_favourites_on_kondo_id"
-    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "kondos", force: :cascade do |t|
@@ -63,7 +54,5 @@ ActiveRecord::Schema.define(version: 2020_11_23_165615) do
 
   add_foreign_key "favorites", "kondos"
   add_foreign_key "favorites", "users"
-  add_foreign_key "favourites", "kondos"
-  add_foreign_key "favourites", "users"
   add_foreign_key "kondos", "users"
 end
