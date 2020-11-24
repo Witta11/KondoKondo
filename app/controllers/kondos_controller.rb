bdc8fa91f5c8fee5149186a6a57ee4efae6140f8
Witta Glöckner <alina.glocke@gmail.com>
2020-11-24 12:10:03 +0100

    set up for search bar

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,6 +1,13 @@
 class KondosController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
+
   def index
     @kondos = policy_scope(Kondo).order(created_at: :desc)
+  #   if params[:query].present?
+  #     @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+  #   else
+  #     @movies = Movie.all
+  #   end
   end
+
 end
