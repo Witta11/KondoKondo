b21beaf21522493b6f98b26ac95052c079bc2a7f
Witta Glöckner <alina.glocke@gmail.com>
2020-11-24 11:37:42 +0100

    merging w master

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,4 +1,5 @@
 class KondosController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
     @kondos = policy_scope(Kondo).order(created_at: :desc)
   end
