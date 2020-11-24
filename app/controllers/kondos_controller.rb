a1d7e1eec0968fe68ef8a0cfc490bf36c3849f90
lennsen <lennjen@gmail.com>
2020-11-24 14:49:59 +0100

    added destroy action in kondos controller

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -10,4 +10,12 @@ class KondosController < ApplicationController
   #   end
   end
 
+  def destroy
+    @kondo = Kondo.find(params[:id])
+    @kondo.user = current_user
+    authorize @kondo
+    @kondo.delete
+    redirect_to kondos_path # redirect has to change to dashboard_path in upcoming version
+  end
+
 end
