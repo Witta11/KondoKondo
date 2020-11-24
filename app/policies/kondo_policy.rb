ab8b38378980f9b6ba349a8c1b7c1930d56e1987
Leif Sehlbach <leif.sehlbach@gmail.com>
2020-11-24 11:21:39 +0100

    added create update destroy methods to KondoPolicy

--- app/policies/kondo_policy.rb
+++ app/policies/kondo_policy.rb
@@ -3,5 +3,18 @@ class KondoPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def create?
+      return true
+    end
+
+    def update?
+      record.user == user
+    end
+
+    def destroy?
+      record.user == user
+    end
+
   end
 end
