2a0a553d0b63df521b87f729f2027417fa3dc975
Matthias LÃuecke <mattisluecke@gmail.com>
2020-11-24 12:51:33 +0100

    added Users and Kondo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,39 @@ require 'faker'
 
 puts "Start seeding Users..."
 
+ 
+Friddi = User.create!(
+    username: "Friddi",
+    first_name: "Friedrich",
+    last_name: "Hegel",
+    email: "#{first_name}.#{last_name}@gmail.com";
+    encrypted_password: "123456"
+)
+
+Kalle = User.create!(
+    username: "Kalle",
+    first_name: "Karl",
+    last_name: "Marx",
+    email: "#{first_name}.#{last_name}@gmail.com";
+    encrypted_password: "123456"
+)
+
+Teddy = User.create!(
+    username: "Teddy",
+    first_name: "Theodor",
+    last_name: "Adorno",
+    email: "#{first_name}.#{last_name}@gmail.com";
+    encrypted_password: "123456"
+)
+
+Fritzi = User.create!(
+    username: "Fritzi",
+    first_name: "Friedrich",
+    last_name: "Nietzsche",
+    email: "#{first_name}.#{last_name}@gmail.com";
+    encrypted_password: "123456"
+)
+
 puts "End seeding Userrs, #{User.count} created ..."
 
 
@@ -20,15 +53,53 @@ puts "Start seeding Kondos..."
 
   Kondo.create!(
     title: "My first Demo tape from 1995",
-    description: "",
+    description: "I originally recorded it when I was still a teenager and made it a Kondo so that another person can have maximum joy of Rap vibes from the past!",
     picture:"https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8dmludGFnZXxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=600&q=60",
     location: "Hagelberger Str. 10-15 10965 Berlin",
-    user: User.all.sample,
+    user: Friddi,
     reserved: false,
-    active: true,
-    created_at: Faker::Date.between(from: 4.days.ago, to: Date.today),
-    updatet_at: 
+    active: true
     )
+
+  Kondo.create!(
+    title: "My deceised Grandma's Grammophone",
+    description: "Although very good in shape, I am emotionally not capable of having it around me anymore, taking money for it feels wrong, so please come by and fetch it ",
+    picture:"https://images.unsplash.com/photo-1518893883800-45cd0954574b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=674&q=80",
+    location: "Kreuzbergstraße 17a, 10965 Berlin",
+    user: Fritzi,
+    reserved: false,
+    active: true
+    )
+
+  Kondo.create!(
+    title: "Vintage Pocket Watch - unfunctional",
+    description: "Just found this very old and sadly not functional watch when tidying my stuff during lock-down, for s.o. to repair maybe? ",
+    picture:"https://images.unsplash.com/photo-1509048191080-d2984bad6ae5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fHZpbnRhZ2V8ZW58MHx8MHw%3D&auto=format&fit=crop&w=600&q=60",
+    location: "Nostitzstraße 15, 10961 Berlin",
+    user: Fritzi,
+    reserved: false,
+    active: true
+    )
+
+  Kondo.create!(
+    title: "Some old maps - posters for you?",
+    description: "Hidden in an old box I found these old maps of Berlin, I kept some for myself, feel free to stop by and get one!",
+    picture:"https://images.unsplash.com/photo-1473163928189-364b2c4e1135?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDJ8fHZpbnRhZ2UlMjBib29rc3xlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    location: "141, Bergmannstraße, 10961 Berlin",
+    user: Kalle,
+    reserved: false,
+    active: true
+    )
+
+#   Kondo.create!(
+#     title: "",
+#     description: "",
+#     picture:"",
+#     location: "",
+#     user: ,
+#     reserved: false,
+#     active: true
+#     )
 
 puts "End seeding Kondos, #{Kondo.count} created ..."
 
