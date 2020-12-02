c61f4c590cd6be5a1534fbea3a6d43f9573e75b6
Matthias LÃuecke <mattisluecke@gmail.com>
2020-12-02 11:22:24 +0100

    getting chatroom deployment ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://www.kondokondo.de/cable"
+  config.action_cable.allowed_request_origins = [ "http://www.kondokondo.de", "https://www.kondokondo.de" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
