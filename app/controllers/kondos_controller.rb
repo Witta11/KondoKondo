98a6c12e0a2ec0209f58e0e24256e889fb551efb
Witta Glöckner <alina.glocke@gmail.com>
2020-12-04 14:21:40 +0100

    adding random to kondo controller skip action bug fixing

--- app/controllers/kondos_controller.rb
+++ app/controllers/kondos_controller.rb
@@ -1,5 +1,5 @@
 class KondosController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :random ]
 
   def index
     if params[:location].present?
